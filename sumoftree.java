faf9f86f710753cbde8772fdb482efcb820d0726
Harshhh2003 <133561905+Harshhh2003@users.noreply.github.com>
2025-04-20 15:17:33 +0530

    sum and height of the tree in java

--- sumoftree.java
+++ sumoftree.java
@@ -1,20 +1,23 @@
-import org.w3c.dom.Node;
-
-public class sumoftree{
-    public static class node{
+public class sumoftree{ public static class node{
         int data;
         node left;
         node right;
         node(int data){
             this.data = data;
         }
-    }
-    public static int sumtree(node root){
+    } public static int sumtree(node root){
         if(root == null) return 0;
         int lefttree = sumtree(root.left);
         int righthalf = sumtree(root.right);
         return lefttree  + righthalf + root.data;
-    }
+    } 
+    public static int heightoftree(node root){
+        if(root == null) return 0;
+        int lefttree = heightoftree(root.left);
+        int righttree = heightoftree(root.right)
+;
+return Math.max(lefttree, righttree)    + 1; // 1 is for root node
+ }
     public static void main(String[] args) {
         node root = new node(10);
         node a = new node(20);
@@ -29,7 +32,8 @@ public class sumoftree{
         
         node e = new node(100);
         c.left = e;
-System.out.println(" total sum of each node  is  :   "   +     sumtree(root));        
+System.out.println(" total sum of each node  is  :   "   +     sumtree(root));   
+System.out.println("height of the tree is   :      "   +   heightoftree(root));     
         
     }
 }
